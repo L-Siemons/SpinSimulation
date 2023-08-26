02b047bf82e5fd204c8d6e18196ee16cea6e0370
Lucas Siemons <lucassiemons@Lucass-MBP.lan>
2023-08-26 17:08:36 +0200

    added the setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+import numpy as np
+
+descrip='''
+====// --------- \\\\====
+Author:
+Lucas Siemons
+====// --------- \\\\====
+'''
+
+setup(
+    name='spinSimulations',
+    version='0.1',
+    author='L. Siemons',
+    author_email='lucas.siemons@googlemail.com',
+    packages=find_packages(),
+    ext_modules=cythonize([
+        "spinSimulations/cython_extensions.pyx", 
+       ]),
+    #license='LICENSE.txt',
+    package_data={'spinSimulations': ['dat/*dat'],  },
+    include_package_data=True,
+    description=descrip,
+    include_dirs=[np.get_include()],
+    zip_safe=False
+    #long_description=open('README.md').read(),
+    #install_requires=['numpy','scipy>=0.17.0', 'matplotlib'],
+)
