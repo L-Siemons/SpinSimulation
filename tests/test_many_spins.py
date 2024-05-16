5b4743af05fc37f1d492da60e132dec65f5a7faf
BOGDAN RODIN <phagost@gmail.com>
2024-05-16 15:33:39 +0200

    System dimensionality test

--- tests/test_many_spins.py
+++ tests/test_many_spins.py
@@ -36,3 +36,14 @@ class TestOpFull:
             sys.op_full('0z1x').toarray(),
             (sys.op(0, 'z') @ sys.op(1, 'x')).toarray()
         )
+        
+    def test_dims(self):
+        ss = s.System(2, nuclei_list=["1H", "2H"])
+        assert 6 == ss.get_spin_dim()
+        
+        ss = s.System(3, nuclei_list=["1H", "2H", "31P"])
+        assert 12 == ss.get_spin_dim()
+        
+        ss = s.System(3, nuclei_list=["1H", "2H", "14N"])
+        assert 18 == ss.get_spin_dim()
+        
